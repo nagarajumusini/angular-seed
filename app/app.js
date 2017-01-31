96e12b7bdb17b71fd70424c8675e36b263817f67
Nagaraju <nagaraju@Nagarajus-MacBook-Pro.local>
2017-01-31 09:05:06 +0530

    modified routeprovider to state provider

--- app/app.js
+++ app/app.js
@@ -2,19 +2,66 @@
 
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
-  'ngRoute',
+  'ui.router',
   'metaservice',
   'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+  'myApp.view2'
 ]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
-  $routeProvider.otherwise({redirectTo: '/view1'});
+config(['$locationProvider', '$urlRouterProvider', '$stateProvider', function($locationProvider, $urlRouterProvider, $stateProvider) {
+  //$locationProvider.hashPrefix('!');
+
+  //$urlRouterProvider.otherwise('/otherwise');
+  $stateProvider
+
+  .state('login', {
+    url: 'login',
+    views: {
+      'content': {
+        template:'<button ui-sref="myapp.aboutus">Go to About us page</button> <br/><button ui-sref="register">got to registration</button>'
+      }
+    }
+  })
+  .state('register', {
+    url: 'register',
+    views: {
+      'content': {
+        template:'<button ui-sref="login">Go to login page</button>'
+      }
+    }
+  })
+  .state('myapp', {
+    views: {
+      'header': {
+        templateUrl:'header/header.html'
+      },
+      'content': {
+        template:'<div ui-view></div>'
+      },
+      'footer': {
+        template:'<hr /> footer'
+      }
+    }
+  })
+  .state('myapp.aboutus', {
+    url: 'aboutus',
+    template:'sup <button ui-sref="myapp.services">Go to services</button>'
+  })
+  .state('myapp.services', {
+    url: 'services',
+    template:'another page <button ui-sref="myapp.contact"> Go to contact</button>'
+  })
+  .state('myapp.contact', {
+    url: 'contact',
+    template:'another page <button ui-sref="login"> Go to login</button>'
+  })
+
+ 
+
 }])
-.run([ '$rootScope', '$location', '$anchorScroll', function( $rootScope, $location, $anchorScroll) {
+.run([ '$rootScope', '$location', '$anchorScroll','$state', function( $rootScope, $location, $anchorScroll, $state) {
   $rootScope.$on("$locationChangeSuccess", function(){
     $anchorScroll();
   });
+  $state.go('login');
 }])
 ;
--- app/view2/view2.js
+++ app/view2/view2.js
@@ -1,12 +1,14 @@
 'use strict';
 
-angular.module('myApp.view2', ['ngRoute'])
+angular.module('myApp.view2', ['ui.router'])
 
-.config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view2', {
+.config(['$stateProvider', function($stateProvider) {
+	$stateProvider
+   .state('myapp.view2', {
+    url: 'view2',
     templateUrl: 'view2/view2.html',
-    controller: 'View2Ctrl'
-  });
+    controller: "View2Ctrl"
+  })
 }])
 
 .controller('View2Ctrl', ['$scope','$rootScope', 'MetaService', function($scope, $rootScope, MetaService) {
